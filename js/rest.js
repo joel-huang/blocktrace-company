e1aff72913074537ba8b2a24df761442802ad4a9
joel-huang <huang.joel@hotmail.com>
2018-03-31 03:16:56 +0800

    pretty done

--- js/rest.js
+++ js/rest.js
@@ -1,6 +1,37 @@
-function getDatabase() {
+var currentJSON;
+var confirmMessage;
+var isErrorShownAlready = false;
 
-    // clear the table
+$('#confirmation').on('hidden.bs.modal', function () {
+    document.getElementById("sorry").style.visibility = "hidden";
+    document.getElementById("delete").disabled = false;
+    isErrorShownAlready = false;
+})
+
+function randQuote() {
+    var qPara = document.getElementById("quote");
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "../res/parsedquotes", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+
+}
+
+function getDatabase() {
+    
+    var button = document.getElementById("getDB");
+    button.disabled = true;
+    button.innerHTML = '<span class="fas fa-sync fa-spin"></span> ' + button.innerHTML;
     var oldTbody = document.getElementById("tablecontent");
     var newTbody = document.createElement("tbody");
     newTbody.id = "hold";
@@ -17,33 +48,107 @@ function getDatabase() {
 
                     var data = xmlHttp.responseText;
                     var parsed = JSON.parse(data);
+                    currentJSON = parsed;
 
-                    for (var entry in parsed) {
+                    for (var i = 0; i < parsed.length; i++) {
+
+                        var username = parsed[i][0];
+                        var password = parsed[i][1];
+                        var nric = parsed[i][2].encrypted_id;
+                        var name = parsed[i][2].name;
+                        var dob = parsed[i][2].birthdate;
+                        var postcode = parsed[i][2].postcode;
 
                         var row = document.createElement("TR");
-                        var request_col = document.createElement("TD");
-                        var priv_key_col = document.createElement("TD");
-                        var pub_key_col = document.createElement("TD");
+                        var nric_col = document.createElement("TD");
+                        var name_col = document.createElement("TD");
+                        var dob_col = document.createElement("TD");
+                        var postcode_col = document.createElement("TD");
+                        var del_col = document.createElement("TD")
                         
-                        request_col.setAttribute("class", "cell100 column1");
-                        priv_key_col.setAttribute("class", "cell100 column2 long");
-                        pub_key_col.setAttribute("class", "cell100 column3 long");
+                        nric_col.setAttribute("class", "cell100 column1");
+                        name_col.setAttribute("class", "cell100 column2");
+                        dob_col.setAttribute("class", "cell100 column3");
+                        postcode_col.setAttribute("class", "cell100 column4");
+                        del_col.setAttribute("class", "cell100 column5")
+
                         row.setAttribute("class", "row100 body");
 
-                        request_col.innerHTML = entry;
-                        priv_key_col.innerHTML = parsed[entry].private_key.slice(48);
-                        pub_key_col.innerHTML = parsed[entry].public_key.slice(71);
+                        nric_col.innerHTML = nric;
+                        name_col.innerHTML = name;
+                        dob_col.innerHTML = dob;
+                        postcode_col.innerHTML = postcode;
+                        del_col.innerHTML = '<a class="times" href="#confirmation" id="trash'+ i +'"onmouseout="mouseLeave(this)" onmouseover="mouseOver(this)" onclick="confirmDelete(this)" data-toggle="modal"><span class="fas fa-times fa-lg"></span></a>';
 
-                        row.appendChild(request_col);
-                        row.appendChild(priv_key_col);
-                        row.appendChild(pub_key_col);
+                        row.appendChild(nric_col);
+                        row.appendChild(name_col);
+                        row.appendChild(dob_col);
+                        row.appendChild(postcode_col);
+                        row.appendChild(del_col);
+
                         document.getElementById("tablecontent").appendChild(row);
                     }
+                document.getElementById("getDB").disabled = false;
+                button.removeChild(button.getElementsByTagName("span")[0]);
             }
         }
-        xmlHttp.open("GET", "https://shielded-bayou-99151.herokuapp.com/get_database", true);
+        xmlHttp.open("GET", "https://shielded-bayou-99151.herokuapp.com/get_company_database", true);
         xmlHttp.send();
     } catch (err) {
         alert(err); 
     }
+}
+
+function confirmDelete(link) {
+    var index = link.id.slice(5);
+    var username = currentJSON[index][0];
+    var name = currentJSON[index][2].name;
+    var nric = currentJSON[index][2].encrypted_id;
+    confirmMessage = 'Are you sure you want to delete user <font style="color: #c70c0c;">'+ username + '</font>: ' + name + ', '  + nric + '? This action cannot be reversed.';
+    console.log("deleting user with username: "+ username);
+    document.getElementById("confirmation-message").innerHTML = confirmMessage;
+    document.getElementById("delete").innerHTML = "Delete " + username;
+}
+
+function mouseLeave(link) {
+    link.getElementsByTagName("span")[0].className = "fas fa-times fa-lg";
+}
+
+function mouseOver(link) {
+    link.getElementsByTagName("span")[0].className = "fas fa-times-circle fa-lg";
+}
+
+function deleteUser(delButton) {
+    var username = delButton.innerHTML.split(" ")[1];
+    delButton.disabled = true;
+    document.getElementById("cancel-delete").disabled = true;
+    document.getElementById("x-delete").disabled = true;
+    delButton.innerHTML = '<span class="fas fa-sync fa-spin"></span> ' + delButton.innerHTML;
+    setTimeout(function() {
+    var xmlHttp = new XMLHttpRequest();
+    try {
+        // i got a response.
+        xmlHttp.onreadystatechange = function() {
+            // response is bad, if error not shown, show error
+            if (xmlHttp.readyState == 4 && xmlHttp.status != 200) {
+                if (!isErrorShownAlready) {
+                    document.getElementById("sorry").style.visibility = "visible";
+                    isErrorShownAlready = true;
+                }
+                delButton.disabled = false;
+            }
+            // response is good
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                isErrorShownAlready = false;
+                $('#confirmation').modal('hide');
+                getDatabase();
+            } 
+            document.getElementById("cancel-delete").disabled = false;
+            document.getElementById("x-delete").disabled = false;
+        }
+        xmlHttp.open("POST", "https://shielded-bayou-99151.herokuapp.com/company_del_user?username=" + username, true);
+        xmlHttp.send();
+    } catch (err) {
+        alert(err); 
+    }    }, 1000);
 }
---
+++ util.py
@@ -0,0 +1,45 @@
+import string
+import random
+import os
+import psycopg2
+import json
+
+def generate_random_string():
+  length = random.randint(5,10)
+  output = ""
+  characters = string.ascii_lowercase
+  for i in range(length):
+    output += random.choice(characters)
+  return output
+
+def connect_db():
+    os.environ['DATABASE_URL'] = "postgres://kulppdlibhsggy:4655032868ea8a3c938e2bd5d015130b41c7810d012e8edc3518de8490bf205d@ec2-54-83-23-91.compute-1.amazonaws.com:5432/db5s1h8iuhepc"
+    conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode = 'require')
+    cur = conn.cursor()
+    return conn, cur
+
+def makenew(num):
+    conn,cur = connect_db()
+    for i in range(1, num):
+        username = generate_random_string()
+        password = "pw"
+        user_info = {"name":generate_random_string(),"birthdate":"23/04/2000","postcode":"485999","encrypted_id":"S1030456F"}
+        cur.execute("INSERT INTO COMPANY_DATABASE (USERNAME,PASSWORD,USER_INFO) \
+                        VALUES (%s,%s,%s)",(username,password,json.dumps(user_info)))
+        conn.commit()
+        print("Done " + str(i))
+
+def del_user(username):
+    conn,cur = connect_db()
+    print("checking for user " + str(username))
+    # cur.execute("DELETE from COMPANY_DATABASE where USERNAME = '%s' + "%username)
+    cur.execute("DELETE from COMPANY_DATABASE)")
+    conn.commit()
+    conn.close()
+    return "Deleted user %s"%username
+
+def del_all():
+    conn,cur = connect_db()
+    cur.execute("DELETE FROM COMPANY_DATABASE")
+    conn.commit()
+    return "Deleted everything"
